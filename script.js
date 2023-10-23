3656b4ae8c5b27aa6f8bf5b109ad6c8f769c5e02
Trent <65882951+Dre-OS@users.noreply.github.com>
2023-10-23 16:44:11 +0800

    initialize CSS version website

---
+++ script.js
@@ -0,0 +1,39 @@
+// letiable instantiation
+const first = 4;
+const sec = 2
+
+// common arithmetic
+function name(params) 
+{
+    let plus = first + sec;
+    document.write(plus);
+    let minus = first - sec;
+    document.write(minus);
+    // prompts user to input a value
+    function name(params) {
+
+    }
+    var age = prompt("please enter you age: ")
+    // if (age>=18) 
+    // { // if statement
+    //     document.write("You are an adult </br>");
+    // }
+    // if (age<18)
+    // { //else statemet
+    //     document.write("You are NOT an adult </br>");
+    // }
+
+    if (age > 18) 
+    {
+    document.write("You are an adult </br>");
+    }
+    else if (age == 18)
+    {
+    document.write("Happy Legality </br>");
+    }
+    else
+    {
+    document.write("You are NOT an adult </br>");
+}
+}
+
